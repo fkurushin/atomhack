b0b7a3bb1086eeecc2320b303bfca35734193f1a
fkurushin <f.kurushin@ya.ru>
2023-05-23 20:13:22 +0300

    read xml/dir

--- rdpackage.py
+++ rdpackage.py
@@ -1,47 +1,53 @@
-# скрипт по формированию Пакета РД;
+import os
 import xml.etree.ElementTree as ET
+from loguru import logger
+import json
 
-# Открываем XML-файл для чтения
-xml_file = "data/8_27_202111_29_00PM/5 9 3 10/AccDocs/CheckList/20211011092922_4DDFC139-4341-4A65-A3CA-D0082D4797B8/4DDFC139-4341-4A65-A3CA-D0082D4797B8.xml"
-tree = ET.parse(xml_file)
-root = tree.getroot()
 
-# Итерируемся по элементам object и создаем объекты на основе метаданных
-for obj in root.findall('object'):
-    # Извлекаем атрибуты объекта
-    obj_id = obj.get('id')
-    create_time = obj.get('createTime')
-    modify_time = obj.get('modifyTime')
-    status = obj.get('status')
-    create_user = obj.get('createUser')
-    modify_user = obj.get('modifyUser')
+# лишние файлы не парные логгировать
 
-    # Создаем словарь со значениями атрибутов
-    attr_dict = {}
-    for attr in obj.find('attributes').findall('attribute'):
-        attr_name = attr.get('name')
-        attr_datatype = attr.get('datatype')
-        attr_value = attr.get('value')
-        attr_dict[attr_name] = {'datatype': attr_datatype, 'value': attr_value}
+def parse_dir(path):
+    result = {}
+    dir_items = os.listdir(path)
 
-    # Создаем список файлов, связанных с объектом
-    files_list = []
-    for file in obj.find('files').findall('file'):
-        file_id = file.get('id')
-        file_name = file.get('name')
-        file_size = file.get('size')
-        file_hash = file.get('hash')
-        file_path = file.get('path')
-        files_list.append({'id': file_id, 'name': file_name, 'size': file_size, 'hash': file_hash, 'path': file_path})
+    for item_name in dir_items:
+        item_path = os.path.join(path, item_name)
 
-    # Выводим созданный объект
-    print({
-        'id': obj_id,
-        'createTime': create_time,
-        'modifyTime': modify_time,
-        'status': status,
-        'createUser': create_user,
-        'modifyUser': modify_user,
-        'attributes': attr_dict,
-        'files': files_list
-    })
+        if os.path.isdir(item_path):
+            result[item_name] = parse_dir(item_path)
+        else:
+            result[item_name] = None
+
+    return result
+
+
+def parse_xml(path):
+    logger.add("std.log", )
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    result_dict = {}
+
+    def find_element(element, dict_obj):
+        for key, value in element.items():
+            dict_obj[key] = value
+
+        if element.text and element.text.strip():
+            dict_obj['value'] = element.text.strip()
+
+        for child_elem in element:
+            child_dict = {}
+            find_element(child_elem, child_dict)
+            dict_obj[child_elem.tag] = child_dict
+
+    find_element(root, result_dict)
+    return result_dict
+
+
+if __name__ == "__main__":
+    dir_path = 'data/8_27_202111_29_00PM'
+    dir_dict = parse_dir(dir_path)
+    with open('directory.json', 'w') as fp:
+        json.dump(dir_dict, fp, indent=4)
+    print(dir_dict)
+    # print(parse_xml("data/8_27_202111_29_00PM/5 9 3 10/AccDocs/CheckList/20211011092922_4DDFC139-4341-4A65-A3CA-D0082D4797B8/4DDFC139-4341-4A65-A3CA-D0082D4797B8.xml"))
