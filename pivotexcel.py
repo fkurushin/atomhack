e5499aafc895a06352744c5dca5f544b860d89b5
fkurushin <f.kurushin@ya.ru>
2023-05-24 13:33:13 +0300

    a lot of funcs

--- pivotexcel.py
+++ pivotexcel.py
@@ -1,1 +1,76 @@
 # скрипт по формированию сводного excel-файла по всем Ведомостям Пакетов РД.
+import os
+# 1) все ок
+# 2) незначительные изменения
+# 3) куча файлов без структуры
+# 4) изменены данные и метаданные
+
+# excel сравнить с документацией с 4 пунктом контрактная спецификация
+
+# exe шник и директорию будет пихать туда
+import csv
+import xml.etree.ElementTree as ET
+from rdpackage import parse_xml
+from loguru import logger
+
+logger.add("std.log", rotation="500 MB")
+logger.info("Session started...")
+
+def check_filename(filename):
+    """
+    Требования из задания: «Файл Ведомости Пакета РД должен соответствовать следующим требованиям:
+    •	иметь код WP в 4-м секторе;
+    •	иметь расширение .doc/.docx;
+    •	оканчиваться на цифру (0,1,2,3 и т.д) – это номер ревизии Ведомости;
+    Дополнительно (необязательно):
+    •	иметь перед цифрой символ r (НЕ e) – это язык ведомости пакета, то есть ведомость на русском»
+
+    Функция для проверки имени файла с ведомостью пакета РД.
+    Возвращает True, если имя файла соответствует требованиям, и False в противном случае.
+    """
+    if not filename.lower().endswith('.doc') and not filename.lower().endswith('.docx'):
+        return False
+
+    if not filename.split(".")[-2][-1].isdigit():
+        return False
+
+    if 'WP' != filename.split()[5]:
+        return False
+
+    return True
+
+
+def xml_to_csv(xml_file, csv_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(['tag_name', 'attribute_name', 'attribute_value'])
+
+        for elem in root.iter():
+            tag_name = elem.tag
+
+            if tag_name in ['attribute', 'row']:
+                attribute_name = elem.attrib.get('name')
+                attribute_value = elem.attrib.get('value', elem.text)
+                writer.writerow([tag_name, attribute_name, attribute_value])
+
+
+def find_doc_in_xml(path:str)->bool:
+    d = parse_xml(path)
+    file_name = d["object"][1]["files"][0]["name"]
+    if check_filename(file_name):
+        logger.info(f"File name '{file_name}' matches the requirements.")
+        return True
+    else:
+        logger.info(f"File name '{file_name}' does not match the requirements.")
+        return False
+
+
+if __name__ == "__main__":
+    # Пример использования функции
+    find_doc_in_xml("data/8_27_202111_29_00PM/5 9 3 10/B3A356AD-6615-4842-9787-7369103404C0.xml")
+    "data/8_27_202111_29_00PM/5 9 3 10/B3A356AD-6615-4842-9787-7369103404C0.xml"
+    xml_to_csv("data/8_27_202111_29_00PM/5 9 3 10/Docs/20211011092925_6C9F52FB-559E-4ECF-968A-1746947824BC/6C9F52FB-559E-4ECF-968A-1746947824BC.xml", "output.csv")
--- rdpackage.py
+++ rdpackage.py
@@ -2,6 +2,10 @@ import os
 import xml.etree.ElementTree as ET
 from functools import wraps
 from loguru import logger
+import os
+import json
+import xmltodict
+from collections import Counter
 import json
 from definitions import STANDART_DIR, TEST_DIR
 
@@ -12,12 +16,15 @@ logger.info("Session started...")
 def log_function(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logger.info(f"Function {func.__name__} called with args={args}, kwargs={kwargs}")
+        logger.info("Function {} called with args={}, kwargs={}", func.__name__, args, kwargs)
+        result = func(*args, **kwargs)
+        logger.info("Function {} returned {}", func.__name__, result)
+        return result
     return wrapper
 
 
 # лишние файлы не парные логгировать
-@log_function
+# @log_function
 def parse_dir(path: str) -> dict:
     """
     Функция рекурсивно парсит директорию path и преобразует
@@ -37,10 +44,12 @@ def parse_dir(path: str) -> dict:
             result[item_name] = None
     return result
 
-def content2extension(path:str)->str:
+
+def content2format(path:str)->str:
     return path.split(".")[-1]
 
-@log_function
+
+# @log_function
 def parse_xml(path: str) -> dict:
     """
     Функция парсит xml файл в словарь словарей
@@ -56,19 +65,73 @@ def parse_xml(path: str) -> dict:
         for key, value in element.items():
             dict_obj[key] = value
 
-        if element.text and element.text.strip():
-            dict_obj['value'] = element.text.strip()
-
-        for child_elem in element:
-            child_dict = {}
-            find_element(child_elem, child_dict)
-            dict_obj[child_elem.tag] = child_dict
+        # Если элемент является списком, то сохраняем атрибуты в виде списка
+        if element.findall('*'):
+            list_dict = []
+            for child_elem in element:
+                child_dict = {}
+                find_element(child_elem, child_dict)
+                list_dict.append(child_dict)
+            dict_obj[element.tag] = list_dict
+        else:
+            # Иначе сохраняем обычный элемент
+            if element.text and element.text.strip():
+                dict_obj['value'] = element.text.strip()
+            for child_elem in element:
+                child_dict = {}
+                find_element(child_elem, child_dict)
+                dict_obj[child_elem.tag] = child_dict
 
     find_element(root, result_dict)
     return result_dict
 
 
-@log_function
+def compare_directory_with_json(dir_path, json_file):
+    # Загружаем JSON-файл в виде словаря
+    expected_structure = parse_dir()
+
+    # Получаем список файлов и каталогов в указанной директории
+    actual_structure = {}
+    for root, dirs, files in os.walk(dir_path):
+        node = actual_structure
+        for dir_name in sorted(dirs):
+            node[dir_name] = {}
+            node = node[dir_name]
+        for file_name in sorted(files):
+            node[file_name] = None
+
+    # Сравниваем ожидаемую структуру с фактической
+    return compare_dicts(expected_structure, actual_structure)
+
+
+def compare_dicts(dict1, dict2):
+    """
+    Рекурсивно сравнивает два словаря, игнорируя ключи, начинающиеся с '_'
+    """
+    for key in dict1:
+        if key.startswith('_'):
+            continue
+        if key not in dict2:
+            return False
+        if isinstance(dict1[key], dict):
+            if not isinstance(dict2[key], dict):
+                return False
+            if not compare_dicts(dict1[key], dict2[key]):
+                return False
+        elif dict1[key] is None:
+            if dict2[key] is not None:
+                return False
+        else:
+            # Проверяем, соответствует ли расширение файла ожидаемому формату
+            expected_format = dict1[key]
+            actual_format = os.path.splitext(key)[1]
+            if not actual_format.endswith(expected_format):
+                return False
+
+    return True
+
+
+# @log_function
 def compare_dirs(dir1_path: str, dir2_path: str) -> bool:
     """
     Функция сравнивает две директории на соответствие структуре и содержанию пакету РД.
@@ -82,13 +145,20 @@ def compare_dirs(dir1_path: str, dir2_path: str) -> bool:
     dir2_content = parse_dir(dir2_path)
 
     if len(dir1_content) != len(dir2_content):
-        logger.info(f"{dir1_content} contains {len(dir1_content)}, "
-                    f"but {dir2_content} contains {len(dir2_content)}")
+        logger.info(f"{dir1_path} contains {len(dir1_content)}, "
+                        f"but {dir2_path} contains {len(dir2_content)}")
 
+    f1 = [content2format(d) for d in dir1_content]
+    f2 = [content2format(d) for d in dir2_content]
+
+    for item in f1:
+        if item not in f2:
+            logger.info(f"{item} is in list1, but not in list2")
 
     # Сверяем наличие элементов и их содержание в двух директориях
     for item_name in dir1_content:
         if item_name not in dir2_content:
+            return False
 
         item1_content = dir1_content[item_name]
         item2_content = dir2_content[item_name]
@@ -96,15 +166,67 @@ def compare_dirs(dir1_path: str, dir2_path: str) -> bool:
         # Если элемент является директорией, рекурсивно вызываем функцию сравнения для него
         if isinstance(item1_content, dict):
             if not compare_dirs(os.path.join(dir1_path, item_name), os.path.join(dir2_path, item_name)):
-                logger.info()
-        # Если элемент является файлом, проверяем совпадение их содержимого
+                return False
         else:
             if item1_content != item2_content:
-                logger.info()
+                return False
 
     return True
 
 
-if __name__ == "__main__":
+def compare_directory_with_json(dir_path, json_file, log_file):
+    # Load the expected directory structure from JSON file
+    with open(json_file) as f:
+        expected_structure = json.load(f)
+        print(expected_structure)
 
-   compare_dirs(STANDART_DIR, TEST_DIR)
+    # Walk through the actual directory structure and store it in a dictionary
+    actual_structure = {}
+    for root, dirs, files in os.walk(dir_path):
+        node = actual_structure
+        for dir_name in sorted(dirs):
+            node[dir_name] = {}
+            node = node[dir_name]
+        for file_name in sorted(files):
+            node[file_name] = None
+
+    compare_dicts(expected_structure, actual_structure)
+
+
+def compare_dicts(expected_dict, actual_dict, path=''):
+    for key in expected_dict:
+        expected_item = expected_dict[key]
+
+        # Ignore keys that start with an underscore
+        if key.startswith('_'):
+            continue
+        print(actual_dict)
+        if key not in actual_dict:
+            logger.warning(f'{path}/{key} is missing from the actual directory')
+            continue
+
+        actual_item = actual_dict[key]
+        if isinstance(expected_item, dict) and isinstance(actual_item, dict):
+            compare_dicts(expected_item, actual_item, f'{path}/{key}')
+        elif expected_item is None:
+            if actual_item is not None:
+                logger.warning(f'{path}/{key} should be a file but is a directory')
+        else:
+            expected_extension = os.path.splitext(key)[1]
+            if not actual_item:
+                logger.warning(f'{path}/{key} is missing from the actual directory')
+            elif not actual_item.endswith(expected_extension):
+                logger.warning(f'{path}/{key} has an incompatible file format: {actual_item}')
+
+        # Remove the item from the actual_dict so we can detect extra items
+        del actual_dict[key]
+
+    for key in actual_dict:
+        if key.startswith('_'):
+            continue
+        logger.warning(f'{path}/{key} is an unexpected file or directory in the actual directory')
+
+
+if __name__ == "__main__":
+   compare_directory_with_json(TEST_DIR)
+
